d58090c11d9b8508f11eaa79dba23c5be630b2f8
iditk <iditked@gmail.com>
2024-07-14 10:56:28 +0300

    Add input field and button to App.js

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter a name" />
+        <Button title="Add name" />
+      </View>
+      <View>
+        <Text>Names</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
+    inputContainer: {
+    flexDirection: 'row',
+    }
 });
